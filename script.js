16c04e7360abf1fd7965c32f2a5d57e2b833412b
liryelaguiar57 <liryelaguiar57@gmail.com>
2026-06-11 16:44:56 -0300

    subindo -ds

---
+++ script.js
@@ -0,0 +1,8 @@
+const tabs = document.querySelectorAll(".tab");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((item) => item.classList.remove("active"));
+    tab.classList.add("active");
+  });
+});
